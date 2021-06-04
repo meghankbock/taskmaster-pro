3f9f1f7ae0ed4827c10e153f8de3d95bdcd75787
Meghan Bock <meghan.bock@gmail.com>
2021-06-04 00:12:20 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,6 @@ var createTask = function (taskText, taskDate, taskList) {
 var auditTask = function (taskEl) {
   // get date from task elemnt
   var date = $(taskEl).find("span").text().trim();
-  // ensure it worked
-  console.log(date);
 
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
@@ -79,7 +77,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -195,13 +193,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the task data in
@@ -238,16 +241,22 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
 });
 
 $("#modalDueDate").datepicker({
   minDate: 1,
 });
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000*60)*30);
 
 // load tasks for the first time
 loadTasks();
